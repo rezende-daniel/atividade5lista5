4aca5c8af5822e916f01011b7bc70314a1bd0afd
daniel-rezende <rezendedaniel2002@gmail.com>
2022-12-06 18:14:39 -0300

    first commit

---
+++ atv5.java
@@ -0,0 +1,16 @@
+//Faça um programa com uma função chamada somaImposto.
+// A função possui dois parâmetros formais: taxaImposto, 
+//que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. 
+//A função “altera” o valor de custo para incluir o imposto sobre vendas
+
+
+
+import java.util.Scanner;
+
+public class atv5fazer {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira o primeiro número:");
+        //int num1 =entrada.nextInt();
+        entrada.close();
+    }}
